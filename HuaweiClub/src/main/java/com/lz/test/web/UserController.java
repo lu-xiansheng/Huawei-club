9916dabb93c38f51660f360da2f170a21226ac73
lz <819468309@qq.com>
2016-09-15 11:52:50 +0800

    加入测试代码，测试成功

---
+++ HuaweiClub/src/main/java/com/lz/test/web/UserController.java
@@ -0,0 +1,14 @@
+package com.lz.test.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping( "/test")
+public class UserController {
+
+	@RequestMapping("/hi")
+	public String test() {
+		return "test";
+	}   
+}  
